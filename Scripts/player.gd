af2d152159265a57ae376d943411d8abcdf1f969
Arthur <arsbr.souza@gmail.com>
2017-10-21 15:58:20 -0200

    Unify movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,15 +1,27 @@
-extends Area2D
+extends Node2D
 
 export var speed = 150
 var moveType = 0
 var dir = Vector2(0,-1)
 export var angle = 5
+export var stealthSpeed = 75
+export var changeDelay = 1
+var delay = changeDelay
 
 func _ready():
 	set_process(true)
 	pass
 	
 func _process(delta):
+	delay -= delta
+	if Input.is_action_pressed("ui_select"):
+		if moveType == 0 and delay <= 0:
+			moveType = 1
+			delay = changeDelay
+		elif moveType == 1 and delay <= 0:
+			moveType = 0
+			delay = changeDelay
+	
 	if moveType == 0:
 		if Input.is_action_pressed("ui_left"):
 			rotate(angle * delta)
@@ -20,5 +32,15 @@ func _process(delta):
 			dir = dir.rotated(-angle * delta)
 		
 		set_pos(get_pos() + dir * speed * delta)
-	
+	elif moveType == 1:
+		var actual_pos = get_pos()
+		if Input.is_action_pressed("ui_left"):
+			actual_pos.x -= stealthSpeed * delta
+		if Input.is_action_pressed("ui_right"):
+			actual_pos.x += stealthSpeed * delta
+		if Input.is_action_pressed("ui_up"):
+			actual_pos.y -= stealthSpeed * delta
+		if Input.is_action_pressed("ui_down"):
+			actual_pos.y += stealthSpeed * delta
+		set_pos(actual_pos)
 	pass
